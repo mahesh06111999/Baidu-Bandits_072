6327a8a9798edb9be24bf1067a3e734a69fd4ea6
mahesh06111999 <maheshsr611@gmail.com>
2024-06-12 11:03:32 +0530

    RightSide Section added

--- Baidu-Bandits/src/App.jsx
+++ Baidu-Bandits/src/App.jsx
@@ -1,12 +1,13 @@
 import AllRoutes from './AllPages/AllRoutes';
 import './App.css';
 import Navbar from './Components/Navbar';
-
+import RightSideBox from './Components/RightSideBox';
 function App() {
   return (
     <div style={{ display: 'flex' }}>
       <Navbar />
       <AllRoutes />
+      <RightSideBox />
     </div>
   );
 }
