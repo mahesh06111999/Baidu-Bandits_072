f43c391b3833c1d72e9eb4005813c373fde6730d
subhankarbanik <subhankarbanik4msbu@gmail.com>
2024-06-13 10:18:10 +0530

    modification done

--- Baidu-Bandits/src/AllPages/Dashboard.jsx
+++ Baidu-Bandits/src/AllPages/Dashboard.jsx
@@ -4,13 +4,15 @@ import { ChakraProvider, Box, Heading, Text, Image, Button, Grid, GridItem } fro
 const Dashboard = () => {
   return (
   <>
-  
     <ChakraProvider>
-      <Box bg='#12A5BC' w='100%' p={4} color='white' >
-        <Grid templateColumns="2fr 1fr" gap={4}>
+      {/* main box */}
+    <Box maxHeight="100vh" overflow="hidden">
+      {/* first box */}
+      <Box bg='#12A5BC' w='100%' p={4} color='white'  borderRadius={"lg"} marginLeft={4} marginRight={4} maxWidth={"96%"} boxShadow="lg">
+        <Grid templateColumns="2fr 1fr" gap={4} >
           <GridItem>
             <Box mt={4}>
-              <Text fontSize="lg">Good Morning, User</Text>
+              <Text fontSize="lg">Hello, User</Text>
               <Heading as="h3" size="lg" mt={2}>
                 Welcome Back!
               </Heading>
@@ -30,40 +32,53 @@ const Dashboard = () => {
           </GridItem>
         </Grid>
       </Box>
+      {/* second box */}
       <Box bg='##F8F8F8' w='100%' p={4} color='Bold'>
-      <Grid templateColumns="1fr 1fr 2fr" gap={4}>
-       <GridItem bgImage="https://img.freepik.com/premium-psd/3d-sport-nutrition-fitness-supplement-fitness-health-exercise-equipment-icon-isolated-yellow-background-3d-rendering-illustration-clipping-path_696265-2353.jpg?w=1060" bgSize="cover" 
-  bgPosition="center" p={2} rounded="md" boxShadow="dark-lg" >
-          <Box mt={4} border={0}  >
-            <Heading as="h3" size="med" mt={2} >
-               Calories
-            </Heading>
-            <Heading as="h1" size="lg" mt={2}>
-                753
-            </Heading>
-            <Text mt={2}>
-                kcal
-             </Text>
-          </Box>
+       <Grid templateColumns="1fr 1fr 2fr" gap={4}>
+        <GridItem p={2} rounded="md" boxShadow="xl" >
+         <Grid templateColumns="1fr 1fr">
+            <GridItem>
+                <Box mt={4} border={0}  >
+                    <Heading as="h3" size="med" mt={2} >
+                      Calories
+                    </Heading>
+                    <Heading as="h1" size="lg" mt={5}>
+                        753
+                    </Heading>
+                    <Text >
+                        kcal
+                    </Text>
+                  </Box>
+            </GridItem>
+            <GridItem>
+              <Image mt={4} src='https://img.freepik.com/premium-psd/3d-sport-nutrition-fitness-supplement-fitness-health-exercise-equipment-icon-isolated-yellow-background-3d-rendering-illustration-clipping-path_696265-2353.jpg?w=1060'></Image>
+            </GridItem>
+         </Grid> 
+         <Image src='https://signalvnoise.com/assets/svn3/images/2016/11/1Eq40iwcboRFBMF37oAaM7Q.png'></Image> 
         </GridItem>
-        <GridItem bgImage={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLq7tKyxTB0F0aMDd-aG4IPf0WWfirhr5P9wEwIbZ3b-Y5mk5C_aY3aSpFDsRh3h91BOU&usqp=CAU"} bgSize="cover" 
-          bgPosition="center" p={2} rounded="md" boxShadow="dark-lg" >
-          <Box mt={4} border={0} >
-            <Heading as="h3" size="med" mt={2} >
-               Walk Rate
-            </Heading>
-            <Heading as="h1" size="lg" mt={2}>
-                1000
-            </Heading>
-            <Text mt={2}>
-                Steps
-             </Text>
-
-          </Box>
-          
+        <GridItem p={2} rounded="md" boxShadow="2xl" >
+         <Grid templateColumns="1fr 1fr">
+            <GridItem>
+                <Box mt={4} border={0}  >
+                    <Heading as="h3" size="med" mt={2} >
+                      Walk Rate
+                    </Heading>
+                    <Heading as="h1" size="lg" mt={5}>
+                        1270
+                    </Heading>
+                    <Text >
+                        steps
+                    </Text>
+                  </Box>
+            </GridItem>
+            <GridItem>
+              <Image mt={4} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLq7tKyxTB0F0aMDd-aG4IPf0WWfirhr5P9wEwIbZ3b-Y5mk5C_aY3aSpFDsRh3h91BOU&usqp=CAU'></Image>
+            </GridItem>
+         </Grid> 
+         <Image src='https://signalvnoise.com/assets/svn3/images/2016/11/1Eq40iwcboRFBMF37oAaM7Q.png'></Image> 
         </GridItem>
         <GridItem bgImage={"https://media.istockphoto.com/id/1325168725/vector/young-woman-jane-standing-smiling-pointing-to-timer-time-set-timing-self-organization-day.jpg?s=612x612&w=0&k=20&c=Ovl0WoVrkYdDY1WT1msMGeNnc6VJP8o9XA8hM5uxkgI="} bgSize="cover" 
-           bgPosition="center" p={2} rounded="md" boxShadow="dark-lg">
+           bgPosition="center" p={2} rounded="md" boxShadow="2xl">
           <Box mt={4} border={0} >
           <Heading as="h3" size="lg" mt={2} mb={20} >
               Fitness Timer
@@ -82,12 +97,11 @@ const Dashboard = () => {
               </GridItem> 
           </Grid>
           </Box>
-          
         </GridItem>
       </Grid>
       <Grid templateColumns="2fr 1fr" gap={4} pt={4}>
       <GridItem  bgSize="cover" 
-           bgPosition="center" p={2} rounded="md" boxShadow="dark-lg" >
+           bgPosition="center" p={2} rounded="md" boxShadow="2xl" >
           <Box mt={4} border={0} >
             <Heading as="h3" size="med" mt={2} >
                Fitnesss Activity
@@ -97,7 +111,7 @@ const Dashboard = () => {
           </Box>
         </GridItem>
         <GridItem bgImage={"https://hips.hearstapps.com/hmg-prod/images/runner-feet-running-on-road-closeup-on-shoe-royalty-free-image-918789438-1553785419.jpg"} bgSize="cover" 
-           bgPosition="center" p={2} rounded="md" boxShadow="dark-lg" >
+           bgPosition="center" p={2} rounded="md" boxShadow="2xl" >
           <Box mt={4} border={0} >
             <Heading as="h3" size="med" mt={2} >
                Reports
@@ -114,7 +128,9 @@ const Dashboard = () => {
         </GridItem>
       </Grid>       
       </Box>
+      </Box>
     </ChakraProvider>
+  
   </>
   );
 };
