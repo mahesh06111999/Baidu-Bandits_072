211496a462c3a43170c0bd960b365b320da9ce6c
mahesh06111999 <maheshsr611@gmail.com>
2024-06-14 09:18:39 +0530

    Bug fixed

--- Baidu-Bandits/src/AllPages/ActivityTracker.jsx
+++ Baidu-Bandits/src/AllPages/ActivityTracker.jsx
@@ -1,6 +1,6 @@
 // ActivityTracker.jsx
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {
   Box,
   Heading,
@@ -17,10 +17,10 @@ import {
   CardFooter,
   CardHeader,
   Center,
-  Input,Select
+  Input,
+  Select,
 } from '@chakra-ui/react';
 import ExerciseRecommendation from './ExerciseRecommendation';
-
 
 import { motion } from 'framer-motion';
 import DynamicForm from '../Components/DynamicForm';
@@ -29,60 +29,62 @@ import Navbar from '../Components/Navbar';
 import { Navigate } from 'react-router';
 import { auth } from '../auth/firebase';
 
-
 // import './animations.css';  // Import the CSS animations
 
 const activities = [
   {
     name: 'Running',
-    image: 'https://img.freepik.com/premium-vector/isometric-illustration-man-measuring-his-performances_130740-698.jpg?w=740',
+    image:
+      'https://img.freepik.com/premium-vector/isometric-illustration-man-measuring-his-performances_130740-698.jpg?w=740',
     progress: 70,
-    description: 'Running is an excellent way to improve cardiovascular health, build endurance, and burn calories. Regular running can help you stay fit, reduce stress, and boost your overall health. Start with shorter distances and gradually increase your mileage to avoid injuries. Always remember to warm up before and cool down after your run.'
+    description:
+      'Running is an excellent way to improve cardiovascular health, build endurance, and burn calories. Regular running can help you stay fit, reduce stress, and boost your overall health. Start with shorter distances and gradually increase your mileage to avoid injuries. Always remember to warm up before and cool down after your run.',
   },
   {
     name: 'Cycling',
-    image: 'https://img.freepik.com/free-photo/professional-cyclist-women_23-2149703293.jpg?t=st=1718180960~exp=1718184560~hmac=42ddf3f296f814a521b7f4065e8ef0001c0d62aa0783c46ae42261d8757cf226&w=740',
+    image:
+      'https://img.freepik.com/free-photo/professional-cyclist-women_23-2149703293.jpg?t=st=1718180960~exp=1718184560~hmac=42ddf3f296f814a521b7f4065e8ef0001c0d62aa0783c46ae42261d8757cf226&w=740',
     progress: 50,
-    description: 'Cycling is a fantastic low-impact exercise that works your lower body muscles and improves your heart health. Whether you cycle outdoors or use a stationary bike, it’s a fun and effective way to stay active. Remember to wear a helmet and follow safety guidelines when cycling outdoors.'
+    description:
+      'Cycling is a fantastic low-impact exercise that works your lower body muscles and improves your heart health. Whether you cycle outdoors or use a stationary bike, it’s a fun and effective way to stay active. Remember to wear a helmet and follow safety guidelines when cycling outdoors.',
   },
   {
     name: 'Swimming',
-    image: 'https://img.freepik.com/free-photo/male-swimmer-swimming-butterfly-stroke_171337-7613.jpg?t=st=1718181057~exp=1718184657~hmac=7013d688afdc067596bbd4a7c8261beedefe41b6d8e4dd535da29f946409dbe2&w=740',
+    image:
+      'https://img.freepik.com/free-photo/male-swimmer-swimming-butterfly-stroke_171337-7613.jpg?t=st=1718181057~exp=1718184657~hmac=7013d688afdc067596bbd4a7c8261beedefe41b6d8e4dd535da29f946409dbe2&w=740',
     progress: 30,
-    description: 'Swimming is a full-body workout that is easy on the joints. It improves cardiovascular fitness, muscle strength, and flexibility. Swimming is also a relaxing activity that can reduce stress and improve your mood. Start with basic strokes and gradually try more advanced techniques as you build confidence.'
+    description:
+      'Swimming is a full-body workout that is easy on the joints. It improves cardiovascular fitness, muscle strength, and flexibility. Swimming is also a relaxing activity that can reduce stress and improve your mood. Start with basic strokes and gradually try more advanced techniques as you build confidence.',
   },
 ];
 
 const ActivityTracker = () => {
   const [showActivities, setShowActivities] = useState(false);
 
-// user data fatch.................
+  // user data fatch.................
   const [data, setdata] = useState();
   const [loading, setLoading] = useState(true);
-  
+
   useEffect(() => {
-    async function fetch(){      
-    const userId=auth?.currentUser?.uid
-  try {
-    if(auth?.currentUser?.uid){
-    const raw=  await getDoc(doc(db, "user",userId ))
-    const solved =raw.data()
-    console.log(solved);
-    setdata(solved)}
-    
-  } catch (error) {
-    console.log(error);
-  }finally{
-    setLoading(false);
-    
-  }
-}  
-fetch()   
+    async function fetch() {
+      const userId = auth?.currentUser?.uid;
+      try {
+        if (auth?.currentUser?.uid) {
+          const raw = await getDoc(doc(db, 'user', userId));
+          const solved = raw.data();
+          console.log(solved);
+          setdata(solved);
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetch();
   }, []);
 
-
-
-  // for form 
+  // for form
   const [showForm, setShowForm] = useState(true);
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const handleShowActivities = () => {
@@ -135,106 +137,118 @@ fetch()
   /////////
   return (
     <>
-    {
-      auth?.currentUser?.email===undefined && <Navigate replace to={"/"}/>
-    }
+      {auth?.currentUser?.email === undefined && <Navigate replace to={'/'} />}
 
-    <div style={{ display: 'flex' }}>
-      <Navbar />
-      <Box width='63%' p={5} height="100vh" overflow="auto">
-      {!showActivities ? (
-        <>
-        <Fade in={!showActivities}>
-          <Box textAlign="center" py={10} px={6}>
-            <Heading as="h2" size="xl" mb={4}>
-              Welcome to Activity Tracker
-            </Heading>
-            <Center>
-              <Image
-                src="https://img.freepik.com/free-vector/sport-fitness-tracker-abstract-concept-vector-illustration-activity-band-health-monitor-wrist-worn-device-application-running-cycling-every-day-training-abstract-metaphor_335657-1454.jpg?t=st=1718180351~exp=1718183951~hmac=48849305ff35333c08fb67262664b70e984691795e8c383900c5b37e1e2d9b9e&w=740"
-                alt="Activity Tracker"
-                boxSize="300px"
-                className="image-hover"
-              />
-            </Center>
-            <Text fontSize="lg" mb={6}>
-              Track your physical activities and stay motivated!
-            </Text>
-            
-            <Button
-              colorScheme="teal"
-              size="lg"
-              onClick={() => setShowActivities(true)}
-              as={motion.button}
-              whileHover={{ scale: 1.1 }}
-              whileTap={{ scale: 0.9 }}
-            >
-              Get Started
-            </Button>
-          </Box>
-        </Fade>
-        
-         </>
-      ) : (
-        <Fade in={showActivities}>
-          <Box>
-            <Heading mb={6} textAlign="center">
-              Activity Tracker
-            </Heading>
-            <Grid templateColumns="repeat(auto-fit, minmax(200px, 1fr))" gap={6}>
-              {activities.map((activity) => (
-                <GridItem
-                  key={activity.name}
-                  borderWidth="1px"
-                  borderRadius="lg"
-                  overflow="hidden"
-                  _hover={{ transform: 'scale(1.05)', transition: '0.3s' }}
-                  as={motion.div}
-                  whileHover={{ scale: 1.05 }}
+      <div style={{ display: 'flex' }}>
+        <Navbar />
+        <Box width="63%" p={5} height="100vh" overflow="auto">
+          {!showActivities ? (
+            <>
+              <Fade in={!showActivities}>
+                <Box textAlign="center" py={10} px={6}>
+                  <Heading as="h2" size="xl" mb={4}>
+                    Welcome to Activity Tracker
+                  </Heading>
+                  <Center>
+                    <Image
+                      src="https://img.freepik.com/free-vector/sport-fitness-tracker-abstract-concept-vector-illustration-activity-band-health-monitor-wrist-worn-device-application-running-cycling-every-day-training-abstract-metaphor_335657-1454.jpg?t=st=1718180351~exp=1718183951~hmac=48849305ff35333c08fb67262664b70e984691795e8c383900c5b37e1e2d9b9e&w=740"
+                      alt="Activity Tracker"
+                      boxSize="300px"
+                      className="image-hover"
+                    />
+                  </Center>
+                  <Text fontSize="lg" mb={6}>
+                    Track your physical activities and stay motivated!
+                  </Text>
+
+                  <Button
+                    colorScheme="teal"
+                    size="lg"
+                    onClick={() => setShowActivities(true)}
+                    as={motion.button}
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
+                  >
+                    Get Started
+                  </Button>
+                </Box>
+              </Fade>
+            </>
+          ) : (
+            <Fade in={showActivities}>
+              <Box>
+                <Heading mb={6} textAlign="center">
+                  Activity Tracker
+                </Heading>
+                <Grid
+                  templateColumns="repeat(auto-fit, minmax(200px, 1fr))"
+                  gap={6}
                 >
-                  <Card height="350px">
-                    <CardHeader>
-                      <Heading size="md">{activity.name}</Heading>
-                    </CardHeader>
-                    <CardBody>
-                      <VStack spacing={2} align="stretch">
-                        <Center>
-                          <Image src={activity.image} alt={activity.name} boxSize="100px" />
-                        </Center>
-                        <Text noOfLines={3}>{activity.description}</Text>
-                        <Progress value={activity.progress} size="sm" colorScheme="green" />
-                        <Text textAlign="center">{activity.progress}% completed</Text>
-                      </VStack>
-                    </CardBody>
-                    <CardFooter>
-                      <Button colorScheme="teal" size="sm">View Details</Button>
-                    </CardFooter>
-                  </Card>
-                </GridItem>
-              ))}
-            </Grid>
+                  {activities.map((activity) => (
+                    <GridItem
+                      key={activity.name}
+                      borderWidth="1px"
+                      borderRadius="lg"
+                      overflow="hidden"
+                      _hover={{ transform: 'scale(1.05)', transition: '0.3s' }}
+                      as={motion.div}
+                      whileHover={{ scale: 1.05 }}
+                    >
+                      <Card height="350px">
+                        <CardHeader>
+                          <Heading size="md">{activity.name}</Heading>
+                        </CardHeader>
+                        <CardBody>
+                          <VStack spacing={2} align="stretch">
+                            <Center>
+                              <Image
+                                src={activity.image}
+                                alt={activity.name}
+                                boxSize="100px"
+                              />
+                            </Center>
+                            <Text noOfLines={3}>{activity.description}</Text>
+                            <Progress
+                              value={activity.progress}
+                              size="sm"
+                              colorScheme="green"
+                            />
+                            <Text textAlign="center">
+                              {activity.progress}% completed
+                            </Text>
+                          </VStack>
+                        </CardBody>
+                        <CardFooter>
+                          <Button colorScheme="teal" size="sm">
+                            View Details
+                          </Button>
+                        </CardFooter>
+                      </Card>
+                    </GridItem>
+                  ))}
+                </Grid>
+              </Box>
+            </Fade>
+          )}
+
+          {/* //form  */}
+          <Box p={5}>
+            {showForm ? (
+              <Fade in={showActivities}>
+                <Box textAlign="center" py={10} px={6}>
+                  <Heading as="h2" size="xl" mb={4}>
+                    Get personalised recommendation !
+                  </Heading>
+                  <DynamicForm />
+                </Box>
+              </Fade>
+            ) : (
+              <ExerciseRecommendation formData={formData} />
+            )}
           </Box>
-        </Fade>
-      )}
-      
-      {/* //form  */}
-      <Box p={5}>
-      {showForm ? (
-         <Fade in={showActivities}>
-          <Box textAlign="center" py={10} px={6}>
-            <Heading as="h2" size="xl" mb={4}>
-             Get personalised recommendation !
-            </Heading>
-            <DynamicForm />
-          </Box>
-        </Fade>
-      ) : (
-        <ExerciseRecommendation formData={formData} />
-      )}
-    </Box>     
-    </Box>
-      <RightSideBox />
-    </div>
+        </Box>
+        <RightSideBox />
+      </div>
     </>
   );
 };
--- Baidu-Bandits/src/AllPages/PersonalTraining.jsx
+++ Baidu-Bandits/src/AllPages/PersonalTraining.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { auth } from '../auth/firebase';
 import Navbar from '../Components/Navbar';
 import RightSideBox from '../Components/RightSideBox';
@@ -8,46 +8,35 @@ const PersonalTraining = () => {
   // user data fatch.................
   const [data, setdata] = useState();
   const [loading, setLoading] = useState(true);
-  
+
   useEffect(() => {
-    async function fetch(){      
-    const userId=auth?.currentUser?.uid
-  try {
-    if(auth?.currentUser?.uid){
-    const raw=  await getDoc(doc(db, "user",userId ))
-    const solved =raw.data()
-    console.log(solved);
-    setdata(solved)}
-    
-  } catch (error) {
-    console.log(error);
-  }finally{
-    setLoading(false);
-    
-  }
-}  
-fetch()   
-  }, []);
-  return( 
-    
-    <>  
-    {
-      auth?.currentUser?.email===undefined && <Navigate replace to={"/"}/>
+    async function fetch() {
+      const userId = auth?.currentUser?.uid;
+      try {
+        if (auth?.currentUser?.uid) {
+          const raw = await getDoc(doc(db, 'user', userId));
+          const solved = raw.data();
+          console.log(solved);
+          setdata(solved);
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
     }
-    <div style={{ display: 'flex' }}>
-    <Navbar />
-    <div style={{width:'63%'}}>
-      Personaltraining
-
-
-    </div>
-    <RightSideBox />
-  </div>
-    
-    
-    
+    fetch();
+  }, []);
+  return (
+    <>
+      {auth?.currentUser?.email === undefined && <Navigate replace to={'/'} />}
+      <div style={{ display: 'flex' }}>
+        <Navbar />
+        <div style={{ width: '63%' }}>Personaltraining</div>
+        <RightSideBox />
+      </div>
     </>
-)
+  );
 };
 
 export default PersonalTraining;
