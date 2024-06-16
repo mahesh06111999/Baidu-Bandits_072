ea44bb7ba5f34abdd83fe5e5fc6fccf79e5d97f0
sanjukumari-tech <foodlazzez@gmail.com>
2024-06-17 03:51:55 +0530

    merge issue resolved

--- Baidu-Bandits/src/Components/Navbar.jsx
+++ Baidu-Bandits/src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import dashboard from '../assets/icons/dash.gif';
 import activity from '../assets/icons/AT.gif';
